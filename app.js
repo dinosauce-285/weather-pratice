afc5c61927d4995b86176311fee182ae388870ed
dinosauce-285 <thanhbualqt@gmail.com>
2022-07-04 21:38:30 +0700

    ok

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ let weatherType = document.querySelector('.weather-type')
 let visionValue = document.querySelector('.visionValue')
 let windValue = document.querySelector('.windValue')
 let humidityPercentage = document.querySelector('.humidityPercentage')
+let time = document.querySelector('.time')
+search.addEventListener('keypress', e => {
+
+  if (e.keyCode == 13) {
+    searchResult = search.value
+  }
+  localStorage.setItem('search', searchResult)
+} )
 async function findLocation() {
-  let lonlatUrl =   `http://api.openweathermap.org/geo/1.0/direct?q=Bristol&appid=6f58277d0ed256acd33d9a45092d6fd1`
+  let searchResult = localStorage.getItem('search')
+  console.log(searchResult);
+  let lonlatUrl =   `http://api.openweathermap.org/geo/1.0/direct?q=${searchResult}&appid=6f58277d0ed256acd33d9a45092d6fd1`
   const locationbApiLink = await fetch(
    lonlatUrl
   );
   const location = await locationbApiLink.json();
+  console.log(location);
   let latNumber = location[0].lat
   let lonNumber = location[0].lon
  
@@ -19,14 +30,17 @@ async function findLocation() {
   const showLocation = await fetch(url)
   const place = await showLocation.json()
   console.log(place);
-city.innerText   = place.name
+city.innerText   = location[0].name
 country.innerText   = place.sys.country
-temp.innerText = (place.main.temp - 273.15).toFixed(1)
+temp.innerText = Math.round((place.main.temp - 273.15)) 
 weatherType.innerText = (place.weather[0].description)
 visionValue.innerText   = place.visibility + ' (m)'
 windValue.innerText = place.wind.speed +' (m/s)'
 humidityPercentage.innerText = place.main.humidity + ' (%)'
-
+let date = new Date().toLocaleString('vi')
+console.log(date);
+time.innerText = date
+localStorage.removeItem('search')
 }
 findLocation();
 
